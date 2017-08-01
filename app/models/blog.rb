72ac778c02dec132a6e996cb406e86004d107358
渡辺健一 <watanabe-kenichi@rouge.plala.or.jp>
2017-08-01 18:17:38 +0900

    mount_uploader コメントアウト

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,6 @@
 class Blog < ActiveRecord::Base
   belongs_to :user
-  mount_uploader :image, ImageUploader
+  # mount_uploader :image, ImageUploader
   has_many :comments, dependent: :destroy
   validates :title, presence: true
   validates :content, presence: true
